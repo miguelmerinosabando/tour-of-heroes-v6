e438cb7422a39a8684900eb7cb4d8b9f6288f5f7
Miguel Merino Sabando <mmerinos@everis.com>
2018-05-23 09:31:49 +0200

    Update some packages, and mocked API

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
 import { Observable } from 'rxjs';
-import { of } from 'rxjs/observable/of';
+import { of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { MessageService } from './message.service';
 
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'; 
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes')
-    return of (HEROES);
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
+
+    getHeroes (): Observable<Hero[]> {
+      return this.http.get<Hero[]>(this.heroesUrl)
+        .pipe(
+          tap(heroes => this.log(`fetched heroes`)),
+          catchError(this.handleError('getHeroes', []))
+        );
+    }
+
+    getHero(id: number): Observable<Hero> {
+      const url = `${this.heroesUrl}/${id}`;
+      return this.http.get<Hero>(url).pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+    }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
-  getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fectched hero id=${id}`); 
-    return of (HEROES.find(hero => hero.id === id)); 
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+   
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+   
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
